f23dc5b1ceefe5059d629de271f0ea025bbb8656
unknown <ebteshamhossain98@gmail.com>
2021-08-18 23:44:47 +0600

    first commit

--- shopping-cart-main/shopping-cart-main/app.js
+++ shopping-cart-main/shopping-cart-main/app.js
@@ -1,11 +1,20 @@
-document.getElementById('case-plus').addEventListener('click', function() {
+function updateCaseNumber(isIncreasing) {
     const caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value;
-    caseInput.value = parseInt(caseNumber) + 1;
-    console.log(caseNumber);
+    let caseNumber = caseInput.value;
+    if (isIncreasing == true) {
+        caseInput.value = parseInt(caseNumber) + 1;
+    } else if (caseNumber > 0) {
+        caseInput.value = parseInt(caseNumber) - 1;
+    }
+    caseInput.value = caseNumber;
+    // update case total
+    const caseTotal = document.getElementById('case-total');
+    caseTotal.innerText = caseNumber * 59;
+}
 
-})
-
+document.getElementById('case-plus').addEventListener('click', function() {
+    updateCaseNumber(true);
+});
 document.getElementById('case-minus').addEventListener('click', function() {
-    const caseInput = document.getElementById('case-number')
+    updateCaseNumber(false)
 })
